f29ac49e51fa651d8d83e45ad7a4f6ef0a5438f7
pilipili96 <pilipili747@gmail.com>
2022-12-22 20:48:57 -0800

    complete assignment 2

--- assignments/02/index.js
+++ assignments/02/index.js
@@ -1,3 +1,43 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+const axios = require('axios');
+
+const API = 'https://jsonplaceholder.typicode.com/posts';
+
+axios.get(API)
+  .then(response => {
+    console.log(response.data.slice(0,5));
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+// const https = require('https');
+
+// const API = 'https://jsonplaceholder.typicode.com/posts';
+
+// https.get(API, (res) => {
+//     let data = '';
+
+//   res.on('data', (d) => {
+//     data += d.toString();
+//   });
+//   res.on('end', (d) => {
+//     const parsedJSON = JSON.parse(data);
+//     console.log(parsedJSON.slice(0,5));
+//   });
+
+// }).on('error', (e) => {
+//   console.error(e);
+// });
+
+
+
+// const request = require('request');
+
+// request('https://jsonplaceholder.typicode.com/posts', { json: true } , (err, res, body) => {
+//   if (err) { return console.log(err); }
+//   console.log(body);
+// //   console.log(body.explanation);
+// });
