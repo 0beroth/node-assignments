54fcc67728ff3026e14acc774335aee17ad5b9ab
pilipili96 <pilipili747@gmail.com>
2022-12-22 19:46:15 -0800

    fix linter errors

--- assignments/01/index.js
+++ assignments/01/index.js
@@ -6,17 +6,16 @@ let system = os.platform();
 const nodeVersion = process.versions.node;
 const availMemory = os.freemem();
 
-switch(system) {
-    case "win32": system = "Windows"
-    break;
-    case "linux": system = "Linux"
-    break;
-    case "darwin": system = "macOs"
-    break; 
-};
+switch (system) {
+case 'win32': system = 'Windows';
+	break;
+case 'linux': system = 'Linux';
+	break;
+case 'darwin': system = 'macOs';
+	break;
+default:
+}
 
 console.log(`Operating system: ${system}`);
 console.log(`Node Version: ${nodeVersion}`);
 console.log(`Available Memory: ${availMemory}`);
-
-
