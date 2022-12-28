77a0576c39bd3e7cb14c3edd976cb02ebfb801a0
pilipili96 <pilipili747@gmail.com>
2022-12-27 19:18:41 -0800

    complete assignment 5

--- assignments/05/index.js
+++ assignments/05/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
+const axios = require('axios');
 
-console.log('hello world');
+const API_1 = 'https://jsonplaceholder.typicode.com/todos/1';
+const API_2 = 'https://jsonplaceholder.typicode.com/todos/2';
+const API_3 = 'https://jsonplaceholder.typicode.com/todos/3';
+const API_4 = 'https://jsonplaceholder.typicode.com/todos/4';
+const API_5 = 'https://jsonplaceholder.typicode.com/todos/5';
+
+Promise.all([axios.get(API_1),
+	axios.get(API_2),
+	axios.get(API_3),
+	axios.get(API_4),
+	axios.get(API_5),
+])
+	.then(axios.spread((...responses) => {
+		for (let i = 0; i < responses.length; i += 1) {
+			const jsonData = JSON.stringify(responses[i].data, null, 2);
+			console.log(jsonData);
+		}
+	}));
