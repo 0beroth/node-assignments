fb84c9da17b4ab8f2dcd698e1f7e0dffc7af4650
pilipili96 <pilipili747@gmail.com>
2022-12-27 18:40:48 -0800

    complete assignment 4

--- assignments/04/index.js
+++ assignments/04/index.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
+require('dotenv').config();
 
-console.log('hello world');
+const axios = require('axios');
+const prompt = require('prompt-sync')({ sigint: true });
+
+let query;
+
+while (!query) {
+	query = prompt('please enter a search query: ');
+}
+
+const API = `https://www.googleapis.com/customsearch/v1?key=${process.env.API}&cx=76254b22e51fd4690&q=${query}`;
+
+axios.get(API)
+	.then((response) => {
+		if (!response.data?.items) {
+			console.log('Your search parameter returns 0 results');
+			return;
+		}
+
+		const output = response.data.items.slice(0, 5).map((item) => ({
+			title: item.title,
+			link: item.link,
+			snippet: item.snippet,
+		}));
+
+		console.log(JSON.stringify(output, null, 2));
+	})
+	.catch((error) => {
+		console.log(error);
+	});
