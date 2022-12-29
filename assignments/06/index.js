d916c97644ce081eabd88402c52679dc1f0f157f
pilipili96 <pilipili747@gmail.com>
2022-12-28 20:00:31 -0800

    assignment 6 first try

--- assignments/06/index.js
+++ assignments/06/index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
+const { spawn } = require('child_process');
 
-console.log('hello world');
+const argument = process.argv[2];
+const options = process.argv.slice(3);
+const command = spawn(argument, options);
+
+command.stdout.on('data', (data) => {
+	console.log(`${data}`); // why `` work
+});
+
+command.on('error', () => {
+	console.log(`/bin/sh: line 1: ${argument}: command not found`);
+});
+
+command.on('exit', () => {
+	console.log('command finished successfully');
+});
