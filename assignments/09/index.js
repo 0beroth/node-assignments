9191d99a5712c600c23e95b45f5c5b1788a72032
pilipili96 <pilipili747@gmail.com>
2023-01-01 21:51:54 -0800

    completed assingment 9

--- assignments/09/index.js
+++ assignments/09/index.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+import got from 'got';
+import * as fs from 'fs';
+
+const url = 'http://ipv4.download.thinkbroadband.com/200MB.zip';
+const fileName = '200MB.zip';
+
+const downloadStream = got.stream(url);
+const fileWriterStream = fs.createWriteStream(fileName);
+
+downloadStream
+	.on('response', () => {
+		process.stdout.write('downloading file... ');
+	})
+	.on('error', (error) => {
+		console.error(`Download failed: ${error.message}`);
+	});
+
+fileWriterStream
+	.on('error', (error) => {
+		console.error(`Could not write file to system: ${error.message}`);
+	})
+	.on('finish', () => {
+		console.log('done');
+	});
+
+downloadStream.pipe(fileWriterStream);
