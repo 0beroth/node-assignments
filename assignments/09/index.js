38a730fff0d45d0aea2557541975d376add7fea5
pilipili96 <pilipili747@gmail.com>
2023-01-03 02:16:36 -0800

    completed assignment 9.5

--- assignments/09/index.js
+++ assignments/09/index.js
@@ -3,26 +3,63 @@
 import got from 'got';
 import * as fs from 'fs';
 
-const url = 'http://ipv4.download.thinkbroadband.com/200MB.zip';
-const fileName = '200MB.zip';
+const downloadFile = async (url, downloadPath, fileNumber) => {
+	const downloadStream = got.stream(url);
+ 	const fileWriterStream = fs.createWriteStream(downloadPath);
+	process.stdout.write(`downloading file ${fileNumber}... `);
+	await new Promise((resolve, reject) => {
+		downloadStream
+			.on('error', (error) => {
+				console.log(`Download failed: ${error.message}`);
+				reject();
+			});
 
-const downloadStream = got.stream(url);
-const fileWriterStream = fs.createWriteStream(fileName);
+		fileWriterStream
+			.on('finish', () => {
+				console.log('done.');
+				resolve();
+			})
+			.on('error', (error) => {
+				console.log(`Could not write file to system: ${error.message}`);
+				reject();
+			});
 
-downloadStream
-	.on('response', () => {
-		process.stdout.write('downloading file... ');
-	})
-	.on('error', (error) => {
-		console.error(`Download failed: ${error.message}`);
+		downloadStream.pipe(fileWriterStream);
 	});
+};
 
-fileWriterStream
-	.on('error', (error) => {
-		console.error(`Could not write file to system: ${error.message}`);
-	})
-	.on('finish', () => {
-		console.log('done');
-	});
+(async () => {
+	try {
+	  await downloadFile('http://ipv4.download.thinkbroadband.com/100MB.zip', '100MB.zip', 1);
+	  await downloadFile('http://ipv4.download.thinkbroadband.com/200MB.zip', '200MB.zip', 2);
+	} catch {
+	   console.error('Error has occured, refer to the error message above');
+	}
+})();
 
-downloadStream.pipe(fileWriterStream);
+// import got from 'got';
+// import * as fs from 'fs';
+
+// const url = 'http://ipv4.download.thinkbroadband.com/200MB.zip';
+// const fileName = '200MB.zip';
+
+// const downloadStream = got.stream(url);
+// const fileWriterStream = fs.createWriteStream(fileName);
+
+// downloadStream
+// 	.on('response', () => {
+// 		process.stdout.write('downloading file... ');
+// 	})
+// 	.on('error', (error) => {
+// 		console.error(`Download failed: ${error.message}`);
+// 	});
+
+// fileWriterStream
+// 	.on('error', (error) => {
+// 		console.error(`Could not write file to system: ${error.message}`);
+// 	})
+// 	.on('finish', () => {
+// 		console.log('done');
+// 	});
+
+// downloadStream.pipe(fileWriterStream);
